064ceebf58fc88f9acfb83ed10e67a69e8957c4c
leif <leifgao@outlook.com>
2016-11-26 21:00:09 +0800

    删除注释

--- src/com/nxd/post/TestDate.java
+++ src/com/nxd/post/TestDate.java
@@ -1,7 +1,6 @@
 package com.nxd.post;
 
 import java.text.DateFormat;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -10,18 +9,6 @@ public class TestDate
 {
 	public static void main(String[] args) throws Exception
 	{
-//		Date d1 = new Date();
-//		System.out.println(d1);
-//		Date d2 = new Date(System.currentTimeMillis()+100000);
-//		System.out.println(d2);
-		
-//		
-//		Calendar c = Calendar.getInstance();
-//		System.out.println(c.get(YEAR));
-//		System.out.println(c.get(MONTH));
-//		System.out.println(c.get(DATE));
-//		System.out.println(c.get(HOUR));
-//		
 		DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		Date d1 = df.parse("2014-10-29 13:29:51");
 		boolean flag = true;
